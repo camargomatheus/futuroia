b569efb8b620a17319ef53a67d119ef1bf8461f3
camargomatheus <162705768+camargomatheus@users.noreply.github.com>
2024-05-24 10:38:46 -0300

    ...

--- script.js
+++ script.js
@@ -6,42 +6,43 @@ const TextoResultado = document.querySelector(".texto-resulado");
 
 const perguntas = [
     {
-        enunciado: "A tecnologia é cada vez mais presente no nosso dia a dia, sobre a utilização da Inteligencia Artificial nas escolas, qual a sua opinião?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
-            "Boa iniciativa",
-            "Ruim, pois pode comprometer o esforço dos alunos"
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
         ]
     },
     {
-        enunciado: "Você faz uso constante da Inteligência Artificial para realizar atividades em seu cotidiano?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
-            "SIm",
-            "Não"
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
         ]
-        {
-            enunciado: "Na sua opinião, a Inteligência Artificial pode ser uma ameaça para o mercado de trabalho futuro?",
-            alternativas: [
-                "Sim, pois ela deve contribuir na perca de trabalhos manuais e exigir mais capacitação",
-                "Não, existem trabalhos que somente humanos conseguem operar"
-            ]
-            {
-                enunciado: "A Inteligência Artifical dificulta o processo de aprendizado?",
-                alternativas: [
-                    "Não, pois ela ajuda a estudar",
-                    "Sim, ela tende deixar os estudantes mais preguiçosos"
-                ]
-            },
-        ];
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?
+    ",
+        alternativas: [
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Criar uma imagem utilizando um gerador de imagem de IA."
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+        ]
+    }
+]
 
-let atual = 0;
-let perguntaAtual;
-
-function mostraPergunta () {
-    PerguntaAtual = perguntas[atual];
-    CaixaPerguntas.textContent = perguntaAtual.enunciado;
-}
-
-mostraPergunta();
 
     
 
